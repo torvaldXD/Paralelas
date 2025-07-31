a675f1a13f3dcd23d21dd2ee14fc100b433ee0ed
Sebastian Marulanda <marulanda.sebastian@correounivalle.edu.co>
2025-07-31 00:04:39 -0500

    Ignorar entorno virtual y archivos temporales

--- venv/Lib/site-packages/ray/_version.py
+++
@@ -1,6 +0,0 @@
-# Replaced with the current commit when building the wheels.
-commit = "03491225d59a1ffde99c3628969ccf456be13efd"
-version = "2.48.0"
-
-if __name__ == "__main__":
-    print("%s %s" % (version, commit))
